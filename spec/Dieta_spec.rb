b8c599f94f24508e0f46732eb7c8b4706643887f
Melissa <alu0100884012@ull.edu.es>
2016-11-07 11:39:37 +0000

    Dieta   Menu de dieta     Debe existir un ttulo (FAILED - 1)
    Failures:
    
      1) Dieta Menu de dieta Debe existir un ttulo
         Failure/Error: menu=Dieta.new(['DESAYUNO'])
    
         NoMethodError:
           undefined method `new' for Dieta:Module
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00076 seconds (files took 0.06556 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./spec/Dieta_spec.rb:12 # Dieta Menu de dieta Debe existir un ttulo
    Creando prueba para titulo

--- spec/Dieta_spec.rb
+++ spec/Dieta_spec.rb
@@ -1,6 +1,16 @@
 require "spec_helper"
 require "./lib/dieta.rb"
 
-describe Dieta do
+describe Dieta do 
 
+	context "Menu de dieta" do
+
+		before:each do
+			menu=Dieta.new(['DESAYUNO'])
+		end
+
+		it 'Debe existir un título' do
+			expect(menu.t).to eq(['DESAYUNO'])
+		end
+	end
 end
