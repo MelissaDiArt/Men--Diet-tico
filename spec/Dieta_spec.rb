29fe816f004666fc99bf4e735dd6f8430bda6f29
Melissa <alu0100884012@ull.edu.es>
2016-11-07 12:00:55 +0000

    Dieta   Menu de dieta     Debe existir un ttulo (FAILED - 1)     Debe existir un porcentaje de ingesta diaria (FAILED - 2)
    Failures:
    
      1) Dieta Menu de dieta Debe existir un ttulo
         Failure/Error:
           def initialize(t)
            @t = t
           end
    
         ArgumentError:
           wrong number of arguments (given 2, expected 1)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
      2) Dieta Menu de dieta Debe existir un porcentaje de ingesta diaria
         Failure/Error:
           def initialize(t)
            @t = t
           end
    
         ArgumentError:
           wrong number of arguments (given 2, expected 1)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00177 seconds (files took 0.06816 seconds to load)
    2 examples, 2 failures
    
    Failed examples:
    
    rspec ./spec/Dieta_spec.rb:12 # Dieta Menu de dieta Debe existir un ttulo
    rspec ./spec/Dieta_spec.rb:16 # Dieta Menu de dieta Debe existir un porcentaje de ingesta diaria
    Prueba para ingesta diaria

--- spec/Dieta_spec.rb
+++ spec/Dieta_spec.rb
@@ -6,11 +6,15 @@ describe Dieta do
 	context "Menu de dieta" do
 
 		before :each do
-			@menu = Dieta::Dieta.new(['DESAYUNO'])
+			@menu = Dieta::Dieta.new('DESAYUNO','15%')
 		end
 
 		it 'Debe existir un título' do
-			expect(@menu.t).to eq(['DESAYUNO'])
+			expect(@menu.t).to eq('DESAYUNO')
+		end
+		
+		it 'Debe existir un porcentaje de ingesta diaria' do
+			expect(@menu.ing).to eq('15%')
 		end
 	end
 end
