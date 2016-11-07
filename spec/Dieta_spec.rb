87d43754deb89955ed7069494b3d13644124dc78
Melissa <alu0100884012@ull.edu.es>
2016-11-07 12:10:16 +0000

    Failures:
      1) Dieta Menu de dieta Debe existir un ttulo
         Failure/Error:
           def initialize(t,ing,pl)
            @t, @ing, @pl = t, ing, pl
           end
    
         ArgumentError:
           wrong number of arguments (given 4, expected 3)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
      2) Dieta Menu de dieta Debe existir un porcentaje de ingesta diaria
         Failure/Error:
           def initialize(t,ing,pl)
            @t, @ing, @pl = t, ing, pl
           end
    
         ArgumentError:
           wrong number of arguments (given 4, expected 3)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
      3) Dieta Menu de dieta Debe mostrar un conjunto de platos
         Failure/Error:
           def initialize(t,ing,pl)
            @t, @ing, @pl = t, ing, pl
           end
    
         ArgumentError:
           wrong number of arguments (given 4, expected 3)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
      4) Dieta Menu de dieta Debe mostrar valores nutricionales
         Failure/Error:
           def initialize(t,ing,pl)
            @t, @ing, @pl = t, ing, pl
           end
    
         ArgumentError:
           wrong number of arguments (given 4, expected 3)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00142 seconds (files took 0.06704 seconds to load)
    4 examples, 4 failures
    
    Failed examples:
    
    rspec ./spec/Dieta_spec.rb:12 # Dieta Menu de dieta Debe existir un ttulo
    rspec ./spec/Dieta_spec.rb:16 # Dieta Menu de dieta Debe existir un porcentaje de ingesta diaria
    rspec ./spec/Dieta_spec.rb:20 # Dieta Menu de dieta Debe mostrar un conjunto de platos
    rspec ./spec/Dieta_spec.rb:24 # Dieta Menu de dieta Debe mostrar valores nutricionales
    Prueba para valores nutricionales

--- spec/Dieta_spec.rb
+++ spec/Dieta_spec.rb
@@ -6,7 +6,7 @@ describe Dieta do
 	context "Menu de dieta" do
 
 		before :each do
-			@menu = Dieta::Dieta.new('DESAYUNO','15%',[['Manzana','1 unidad','60gr'],['Pera','1 unidad','60gr']])
+			@menu = Dieta::Dieta.new('DESAYUNO','15%',[['Manzana','1 unidad','60gr'],['Pera','1 unidad','60gr']],[400,10,20,10])
 		end
 
 		it 'Debe existir un título' do
@@ -19,6 +19,10 @@ describe Dieta do
 
 		it 'Debe mostrar un conjunto de platos' do 
 			expect(@menu.pl).to eq([['Manzana','1 unidad','60gr'],['Pera','1 unidad','60gr']])
+		end
+
+		it 'Debe mostrar valores nutricionales' do
+			expect(@menu.v).to eq([400,10,20,10])
 		end
 	end
 end
