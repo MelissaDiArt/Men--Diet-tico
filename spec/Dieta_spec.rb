534ad2f49e5e89152596ef7f7dad80959b5d0bf7
Melissa <alu0100884012@ull.edu.es>
2016-11-07 12:05:27 +0000

    Dieta   Menu de dieta     Debe existir un ttulo (FAILED - 1)     Debe existir un porcentaje de ingesta diaria (FAILED - 2)     Debe mostrar un conjunto de platos (FAILED - 3)
    Failures:
    
      1) Dieta Menu de dieta Debe existir un ttulo
         Failure/Error:
           def initialize(t,ing)
            @t, @ing = t, ing
           end
    
         ArgumentError:
           wrong number of arguments (given 3, expected 2)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
      2) Dieta Menu de dieta Debe existir un porcentaje de ingesta diaria
         Failure/Error:
           def initialize(t,ing)
            @t, @ing = t, ing
           end
    
         ArgumentError:
           wrong number of arguments (given 3, expected 2)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
      3) Dieta Menu de dieta Debe mostrar un conjunto de platos
         Failure/Error:
           def initialize(t,ing)
            @t, @ing = t, ing
           end
    
         ArgumentError:
           wrong number of arguments (given 3, expected 2)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00121 seconds (files took 0.06792 seconds to load)
    3 examples, 3 failures
    
    Failed examples:
    
    rspec ./spec/Dieta_spec.rb:12 # Dieta Menu de dieta Debe existir un ttulo
    rspec ./spec/Dieta_spec.rb:16 # Dieta Menu de dieta Debe existir un porcentaje de ingesta diaria
    rspec ./spec/Dieta_spec.rb:20 # Dieta Menu de dieta Debe mostrar un conjunto de platos
    Creando la prueba para los platos

--- spec/Dieta_spec.rb
+++ spec/Dieta_spec.rb
@@ -6,7 +6,7 @@ describe Dieta do
 	context "Menu de dieta" do
 
 		before :each do
-			@menu = Dieta::Dieta.new('DESAYUNO','15%')
+			@menu = Dieta::Dieta.new('DESAYUNO','15%',[['Manzana','1 unidad','60gr'],['Pera','1 unidad','60gr']])
 		end
 
 		it 'Debe existir un título' do
@@ -15,6 +15,10 @@ describe Dieta do
 		
 		it 'Debe existir un porcentaje de ingesta diaria' do
 			expect(@menu.ing).to eq('15%')
+		end
+
+		it 'Debe mostrar un conjunto de platos' do 
+			expect(@menu.pl).to eq([['Manzana','1 unidad','60gr'],['Pera','1 unidad','60gr']])
 		end
 	end
 end
