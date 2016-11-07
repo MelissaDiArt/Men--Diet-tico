aa35c5194178fe50d1a86cde3e4e1b1ab6cc5a9b
Rafael <alu0100891812@ull.edu.es>
2016-11-07 12:12:36 +0000

    Creando valores nutricionales

--- lib/Dieta/dieta.rb
+++ lib/Dieta/dieta.rb
@@ -1,8 +1,8 @@
 module Dieta
 	class Dieta
-		attr_reader :t, :ing, :pl
-		def initialize(t,ing,pl)
-			@t, @ing, @pl = t, ing, pl
+		attr_reader :t, :ing, :pl, :v
+		def initialize(t,ing,pl,v)
+			@t, @ing, @pl, @v = t, ing, pl, v
 		end
         end
    
