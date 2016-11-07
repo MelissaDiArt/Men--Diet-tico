44f0cac348f0e637a9ec030aa3421dfc726698e6
Rafael <alu0100891812@ull.edu.es>
2016-11-07 12:44:24 +0000

    Creando el metodo to_s

--- lib/Dieta/dieta.rb
+++ lib/Dieta/dieta.rb
@@ -7,6 +7,15 @@ module Dieta
 		def get_plato(i)
 			@pl[i]
 		end
+		def to_s
+			s = "#{@t} (#{@ing})\n"
+			i=0
+			while i<@pl.length
+				s << "- #{@pl[i][0]}, #{@pl[i][1]}, #{@pl[i][2]}\n"
+				i+=1
+			end
+			s << "V.C.T | %\t#{@v} kcal | #{@p}% - #{@g}% - #{@h}%\n"
+		end
         end
    
 end
