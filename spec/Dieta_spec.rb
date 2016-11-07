cc066a59b1d3b7dd3f14f8054029437f7c13e41d
Melissa <alu0100884012@ull.edu.es>
2016-11-07 12:17:51 +0000

    Failure/Error:        def initialize(t,ing,pl,v)         @t, @ing, @pl, @v = t, ing, pl, v        end
         ArgumentError:
           wrong number of arguments (given 7, expected 4)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
      5) Dieta Menu de dieta Debe mostrar proteinas
         Failure/Error:
           def initialize(t,ing,pl,v)
            @t, @ing, @pl, @v = t, ing, pl, v
           end
    
         ArgumentError:
           wrong number of arguments (given 7, expected 4)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
      6) Dieta Menu de dieta Debe mostrar grasas
         Failure/Error:
           def initialize(t,ing,pl,v)
            @t, @ing, @pl, @v = t, ing, pl, v
           end
    
         ArgumentError:
           wrong number of arguments (given 7, expected 4)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
      7) Dieta Menu de dieta Debe mostrar hidratos de carbono
         Failure/Error:
           def initialize(t,ing,pl,v)
            @t, @ing, @pl, @v = t, ing, pl, v
           end
    
         ArgumentError:
           wrong number of arguments (given 7, expected 4)
         # ./lib/Dieta/dieta.rb:4:in `initialize'
         # ./spec/Dieta_spec.rb:9:in `new'
         # ./spec/Dieta_spec.rb:9:in `block (3 levels) in <top (required)>'
    
    Finished in 0.00243 seconds (files took 0.08371 seconds to load)
    7 examples, 7 failures
    
    Failed examples:
    
    rspec ./spec/Dieta_spec.rb:12 # Dieta Menu de dieta Debe existir un ttulo
    rspec ./spec/Dieta_spec.rb:16 # Dieta Menu de dieta Debe existir un porcentaje de ingesta diaria
    rspec ./spec/Dieta_spec.rb:20 # Dieta Menu de dieta Debe mostrar un conjunto de platos
    rspec ./spec/Dieta_spec.rb:24 # Dieta Menu de dieta Debe mostrar kcal
    rspec ./spec/Dieta_spec.rb:28 # Dieta Menu de dieta Debe mostrar proteinas
    rspec ./spec/Dieta_spec.rb:32 # Dieta Menu de dieta Debe mostrar grasas
    rspec ./spec/Dieta_spec.rb:36 # Dieta Menu de dieta Debe mostrar hidratos de carbono
    
    Cambiando vectores por variables

--- spec/Dieta_spec.rb
+++ spec/Dieta_spec.rb
@@ -6,7 +6,7 @@ describe Dieta do
 	context "Menu de dieta" do
 
 		before :each do
-			@menu = Dieta::Dieta.new('DESAYUNO','15%',[['Manzana','1 unidad','60gr'],['Pera','1 unidad','60gr']],[400,10,20,10])
+			@menu = Dieta::Dieta.new('DESAYUNO','15%',[['Manzana','1 unidad','60gr'],['Pera','1 unidad','60gr']],'400','10','20','10')
 		end
 
 		it 'Debe existir un título' do
@@ -21,8 +21,20 @@ describe Dieta do
 			expect(@menu.pl).to eq([['Manzana','1 unidad','60gr'],['Pera','1 unidad','60gr']])
 		end
 
-		it 'Debe mostrar valores nutricionales' do
-			expect(@menu.v).to eq([400,10,20,10])
+		it 'Debe mostrar kcal' do
+			expect(@menu.v).to eq('400')
+		end
+
+		it 'Debe mostrar proteinas' do
+			expect(@menu.p).to eq('10')
+		end
+	
+		it 'Debe mostrar grasas' do
+			expect(@menu.g).to eq('20')
+		end
+
+		it 'Debe mostrar hidratos de carbono' do
+			expect(@menu.h).to eq('10')
 		end
 	end
 end
