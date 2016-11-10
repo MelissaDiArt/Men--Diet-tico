3c73e0996528320c9943e769ca87f3a2f604330e
Melissa <alu0100884012@ull.edu.es>
2016-11-10 16:37:13 +0000

    Implementando el insert

--- lib/Dieta/list.rb
+++ lib/Dieta/list.rb
@@ -2,5 +2,16 @@ Nodo=Struct.new(:v,:s)
 
 class Lista 
 
+	attr_accessor :head
+	
+	def initialize()
+	
+		@head = nil
+	end
+
+	def insert(valor)
+		x = Nodo.new(valor,@head)
+		@head = x
+	end
 
 end 
