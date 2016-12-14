d5aa6a07ff63719931ff49f7ecb83aeeb535252d
Melissa <alu0100884012@ull.edu.es>
2016-12-14 22:09:47 +0000

    Declarada la clase Menu_dls

---
+++ lib/Dieta/dieta_dsl.rb
@@ -0,0 +1,8 @@
+class Menu_dsl
+
+
+
+
+
+
+end
--- lib/dieta.rb
+++ lib/dieta.rb
@@ -1,6 +1,7 @@
 require "Dieta/version"
 require "Dieta/dieta"
 require "Dieta/list"
+require "Dieta/dieta_dsl"
 
 module Dieta
   # Your code goes here...
